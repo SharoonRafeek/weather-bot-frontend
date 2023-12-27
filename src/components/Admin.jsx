d1e6cec631e0821f2770c3aa42e94684bc6c8ab3
SharoonRafeek <sharoonrafeek@gmail.com>
2023-12-27 10:54:16 +0530

    Refactor Admin component navbar

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,29 +1,43 @@
 import { useStore } from "../hooks/useStore";
 import { googleLogout } from "@react-oauth/google";
+
 const Admin = () => {
   const { authData, setAuthData } = useStore();
-  return (
-    <div className={"container"}>
-      {authData && (
-        <>
-          <h1>{authData.data.name}</h1>
-          <p>{authData.data.email}</p>
-          <img src={authData.data.image} alt="profile" />
 
-          <button
-            onClick={() => {
-              googleLogout();
-              localStorage.removeItem("AuthData");
-              setAuthData(null);
-              window.location.reload();
-            }}
-            className={"button"}
-          >
-            Logout
-          </button>
-        </>
-      )}
-    </div>
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="flex items-center">
+          <h1 className="text-white text-lg font-bold">Your Logo</h1>
+        </div>
+
+        {authData && (
+          <div className="flex items-center space-x-4">
+            <div>
+              <h1 className="text-white text-sm">{authData.data.name}</h1>
+              <p className="text-gray-300 text-xs">{authData.data.email}</p>
+            </div>
+            <img
+              className="w-8 h-8 rounded-full"
+              src={authData.data.image}
+              alt="profile"
+            />
+
+            <button
+              onClick={() => {
+                googleLogout();
+                localStorage.removeItem("AuthData");
+                setAuthData(null);
+                window.location.reload();
+              }}
+              className="bg-red-500 text-white px-3 py-1 rounded"
+            >
+              Logout
+            </button>
+          </div>
+        )}
+      </div>
+    </nav>
   );
 };
 

3a1ac6f64d33046fc7788b365cf48c36b85aca1c
SharoonRafeek <sharoonrafeek@gmail.com>
2023-12-27 10:46:51 +0530

    Refactor sign-in page

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,47 @@ import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import axios from "axios";
 import { useStore } from "./hooks/useStore";
 import Users from "./components/Users";
-import Admin from "./components/Admin";
 
 function App() {
   const setAuthData = useStore((state) => state.setAuthData);
   return (
     <div className="App">
       {!useStore((state) => state.authData) ? (
-        <>
-          <h1>Sign In</h1>
-          <GoogleOAuthProvider
-            clientId={import.meta.env.VITE_CLIENT_ID}
-          >
-            <GoogleLogin
-              useOneTap={true}
-              onSuccess={async (credentialResponse) => {
-                console.log(credentialResponse);
-                const { data } = await axios.post(
-                  import.meta.env.VITE_BACKEND_URL + "/login",
-                  {
-                    token: credentialResponse.credential,
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+          <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+            <h1 className="text-3xl font-bold mb-6 text-center text-gray-800">
+              Sign In
+            </h1>
+            <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
+              <GoogleLogin
+                useOneTap={true}
+                onSuccess={async (credentialResponse) => {
+                  console.log(credentialResponse);
+                  try {
+                    const { data } = await axios.post(
+                      import.meta.env.VITE_BACKEND_URL + "/login",
+                      {
+                        token: credentialResponse.credential,
+                      }
+                    );
+                    localStorage.setItem("AuthData", JSON.stringify(data));
+                    setAuthData(data);
+                  } catch (error) {
+                    console.error('Login failed:', error);
                   }
-                );
-                localStorage.setItem("AuthData", JSON.stringify(data));
-                setAuthData(data);
-              }}
-              onError={() => {
-                console.log("Login Failed");
-              }}
-            />
-          </GoogleOAuthProvider>
-        </>
+                }}
+                onError={() => {
+                  console.log("Login Failed");
+                }}
+                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 cursor-pointer w-full"
+              >
+                Sign in with Google
+              </GoogleLogin>
+            </GoogleOAuthProvider>
+          </div>
+        </div>
       ) : (
         <>
-          <Admin />
           <Users />
         </>
       )
